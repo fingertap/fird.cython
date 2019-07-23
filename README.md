8e276fc0c70be689cceb61dc3d497365bb3363f8
Han <zh950713@gmail.com>
2019-07-23 19:39:14 +0800

    Package infos

---
+++ README.md
@@ -0,0 +1,10 @@
+# FIRD: Fraud Detection by Simultaneous Feature Selection and Clustering
+
+This is a [Cython](https://cython.org/) implementation of the clustering algorithm FIRD.
+
+## Installation
+
+```shell
+git clone https://github.com/fingertap/fird.cython.git
+cd fird.cython && pip install .
+```
--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
+import os
 from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
+
+NAME = 'FIRD'
+
+AUTHOR = 'Han Zhang'
+
+AUTHOR_EMAIL = 'zh950713@gmail.com'
+
+GITHUB = 'https://www.github.com/fingertap/fird.cython'
+
+VERSION = '0.0.1'
+
+DESCRIPTION = ('FIRD: Fraud Detection by Simultaneous '
+               'Feature Selection and Clustering')
+
+
+def long_desc():
+    return open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
 
 ext_modules = [
     Extension("fird.fird",
@@ -9,7 +28,14 @@ ext_modules = [
 ]
 
 setup(
-    name="FIRD",
+    name=NAME,
+    version=VERSION,
     ext_modules=cythonize(ext_modules),
-    packages=find_packages()
+    packages=find_packages(),
+    description=DESCRIPTION,
+    long_description=long_desc(),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=GITHUB,
+    license='MIT'
 )
